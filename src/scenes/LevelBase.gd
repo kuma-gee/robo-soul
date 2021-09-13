fe47e31ad4f6e517e762ed75d9abb1adeaac4a05
myin <minyin142p@gmail.com>
2021-09-13 07:57:45 +0200

    Close door after enter

--- src/scenes/LevelBase.gd
+++ src/scenes/LevelBase.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+onready var door_anim := $Door/AnimationPlayer
 
 func _on_TileMap_soul_exited():
 	GUI.open_menu(GUI.GameOver, true)
+
+
+func _on_CloseDoorTrigger_body_exited(body):
+	door_anim.play("Close")
