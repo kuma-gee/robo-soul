3d85b6a67b31fdb03addf6a08d7cc5490c2d8c3d
myin <minyin142p@gmail.com>
2021-09-16 19:14:07 +0200

    ci: package with game name

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 PLATFORM=$1
+NAME=${2:-unnamed}
 
 if [ -z $PLATFORM ]; then
     echo "Platform not specified"
     exit
 fi
 
+# Mac should already be a zip file
 if [ "$PLATFORM" != "mac" ]; then
-    cd build/$PLATFORM && zip -r $PLATFORM.zip *
+    cd build
+    mv $PLATFORM $NAME && zip -r $NAME-$PLATFORM.zip $NAME
 fi
