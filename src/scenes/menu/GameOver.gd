a3650ddd8467044ae1fb6793d3fda0660e67d058
myin <minyin142p@gmail.com>
2021-09-16 19:25:47 +0200

    feat: enable retry shortcut for gameover screen

--- src/scenes/menu/GameOver.gd
+++ src/scenes/menu/GameOver.gd
@@ -4,3 +4,8 @@ onready var sound := $GameOverSound
 
 func _ready():
 	sound.play()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("reload_scene"):
+		get_tree().reload_current_scene()
+		GUI.open_menu(GUI.InGame, true)
